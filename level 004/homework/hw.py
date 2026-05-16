1f963ba48258736279ae24733e7380f62197a1cb
Sandrosakhuria <sandrosaxuria@gmail.com>
2026-05-16 14:32:06 +0400

    second commit

---
+++ level 004/homework/hw.py
@@ -0,0 +1,25 @@
+#თქვენი სიტვყებით ახსენით რა არის debuging?
+#debugging არის შეცდომის გასწორება
+
+#როგორ კითხულობს პროგრმა ჩვენს კოდს?
+#ზევიდან ქვემოთ
+#ქვემოდან ზემოთ
+# კომპიუტერი კითხულობს 'ზევიდან ქვემოთ'
+
+#)გაასწორეთ ეს კოდი
+#input(enter your name
+#input('enter your last name')
+
+#თქვენი სიტყვებით ახსენით რაში დაგვჭირდება input?
+#input ჩვენ input ით ინფორმაცია შევიყვანოთ
+
+#მომხარებელს შეეკითხეთ სახელი და გვარი,ამის შემდეგ კი ეკრანზე გამოიტანეთ ორივე
+user_name=input('enter your name: ')
+user_lastname=input('enter your lastname: ')
+print(user_name)
+print(user_lastname)
+
+#)შექმენი 2 ცვლადი name,surname,age,სადაც მომხარებელს შეეკითხებით შესაბამის კითხვებს,ბოლოს კი გამოიტანს ეკრანზე პასუხებს
+user_name1=input('enter your name: ')
+user_surname=input('enter your surname: ')
+user_age1=input('enter your age: ')
